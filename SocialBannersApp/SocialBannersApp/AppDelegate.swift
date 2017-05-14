1f7fef81294293e340d01b59cfd33eb132d2d79a
Ruslan Timchenko <rustimchek@mail.ru>
2017-05-14 14:45:01 +0300

    init

---
+++ SocialBannersApp/SocialBannersApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SocialBannersApp
+//
+//  Created by Ruslan Timchenko on 14.05.17.
+//  Copyright © 2017 Ruslan Timchenko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SocialBannersApp/SocialBannersApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SocialBannersApp
+//
+//  Created by Ruslan Timchenko on 14.05.17.
+//  Copyright © 2017 Ruslan Timchenko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
